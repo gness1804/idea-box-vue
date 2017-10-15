96c36a569835acf23dfb87c733dae28e9cff7d83
gness1804 <graham.nessler@gmail.com>
2017-10-15 17:06:32 -0500

    Add more styling

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -6,24 +6,28 @@
         type="text"
         placeholder="Idea Name"
         v-model="name"
+        v-bind:style="inputText"
       />
       <input
         type="text"
         placeholder="Idea Body"
         v-model="body"
+        v-bind:style="inputText"
       />
       <p>Quality:</p>
-      <select v-model="quality">
+      <select v-model="quality" v-bind:style="select">
         <option>Swill</option>
         <option>Plausible</option>
         <option>Genius</option>
       </select>
-      <button v-on:click="submitIdea">Enter Idea</button>
+      <button v-on:click="submitIdea" v-bind:style="mainSubmitButton">Enter Idea</button>
     </div>
   </div>
 </template>
 
 <script>
+import styles from '../styles/Landing-styles';
+
 export default {
   name: 'Landing',
   data() {
@@ -33,6 +37,9 @@ export default {
       name: '',
       body: '',
       quality: 'Swill',
+      inputText: styles.inputText,
+      select: styles.select,
+      mainSubmitButton: styles.mainSubmitButton,
     };
   },
   methods: {
---
+++ src/styles/Landing-styles.js
@@ -0,0 +1,18 @@
+const styles = {
+  inputText: {
+    display: 'block',
+    margin: '20px auto',
+  },
+  mainSubmitButton: {
+    border: '1px solid #1cd9cf',
+    borderRadius: 0,
+    fontSize: '20px',
+    padding: '10px',
+  },
+  select: {
+    display: 'block',
+    margin: '20px auto',
+  },
+};
+
+export default styles;
